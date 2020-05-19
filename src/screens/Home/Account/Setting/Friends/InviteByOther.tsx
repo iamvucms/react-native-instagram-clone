72eb849d9b81067c282c0d4960ac87cfa7fd2251
Vucms <vuzero007@gmail.com>
2020-05-19 22:43:09 +0700

    Done Follow and Friends setting

--- src/screens/Home/Account/Setting/Friends/InviteByOther.tsx
+++ src/screens/Home/Account/Setting/Friends/InviteByOther.tsx
@@ -4,9 +4,11 @@ import { useRoute } from '@react-navigation/native'
 import NavigationBar from '../../../../../components/NavigationBar'
 import { navigation } from '../../../../../navigations/rootNavigation'
 import { settingNavigationMap, SettingNavigation } from '../../../../../constants'
+import Share, { Options } from 'react-native-share'
+import { useSelector } from '../../../../../reducers'
 const InviteByOther = (): JSX.Element => {
     const route = useRoute()
-
+    const user = useSelector(state => state.user.user.userInfo)
     const [currNavigation, setCurrNavigation] =
         useState<SettingNavigation | { name: string }>({ name: '' })
     useEffect(() => {
@@ -22,7 +24,58 @@ const InviteByOther = (): JSX.Element => {
             }
             return true;
         })
+        _onSharePost()
     }, [])
+    const _onSharePost = () => {
+        const options: Options = {
+            activityItemSources: [
+                { // For sharing url with custom title.
+                    placeholderItem: {
+                        type: 'url',
+                        content: 'https://www.facebook.com/photo.php?fbid=619895371910790'
+                    },
+                    item: {
+                        default: { type: 'url', content: 'https://www.facebook.com/photo.php?fbid=619895371910790' },
+                    },
+                    subject: {
+                        default: '',
+                    },
+                    linkMetadata: {
+                        originalUrl: 'https://www.facebook.com/photo.php?fbid=619895371910790',
+                        url: 'https://www.facebook.com/photo.php?fbid=619895371910790',
+                        // title: item.content
+                    },
+                },
+                { // For sharing text.
+                    placeholderItem: { type: 'text', content: "" },
+                    item: {
+                        default: { type: 'text', content: `I'm on Instagram as @${user?.username}. Install the app to follow my photos and videos.` },
+                        message: null, // Specify no text to share via Messages app.
+                    },
+                    linkMetadata: { // For showing app icon on share preview.
+                        title: `https://img.favpng.com/9/25/24/computer-icons-instagram-logo-sticker-png-favpng-LZmXr3KPyVbr8LkxNML458QV3.jpg`
+                    },
+                },
+                { // For using custom icon instead of default text icon at share preview when sharing with message.
+                    placeholderItem: {
+                        type: 'url',
+                        content: `I'm on Instagram as @${user?.username}. Install the app to follow my photos and videos.`
+                    },
+                    item: {
+                        default: {
+                            type: 'text',
+                            content: `I'm on Instagram as @${user?.username}. Install the app to follow my photos and videos.`
+                        },
+                    },
+                    linkMetadata: {
+                        title: `I'm on Instagram as @${user?.username}. Install the app to follow my photos and videos.`,
+                        icon: `https://img.favpng.com/9/25/24/computer-icons-instagram-logo-sticker-png-favpng-LZmXr3KPyVbr8LkxNML458QV3.jpg`
+                    }
+                },
+            ],
+        }
+        Share.open(options)
+    }
     return (
         <SafeAreaView style={styles.container}>
             <NavigationBar title={(currNavigation as { name: string }).name} callback={() => {
--- src/screens/Home/Account/Setting/Friends/InviteBySMS.tsx
+++ src/screens/Home/Account/Setting/Friends/InviteBySMS.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { StyleSheet, Linking, View, SafeAreaView, ScrollView, TouchableOpacity, Image, Text } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import NavigationBar from '../../../../../components/NavigationBar'
 import { navigation } from '../../../../../navigations/rootNavigation'
-import { settingNavigationMap, SettingNavigation } from '../../../../../constants'
+import { settingNavigationMap, SettingNavigation, SCREEN_WIDTH } from '../../../../../constants'
+import { useSelector } from '../../../../../reducers'
 const InviteBySMS = (): JSX.Element => {
     const route = useRoute()
-
+    const user = useSelector(state => state.user.user.userInfo)
     const [currNavigation, setCurrNavigation] =
         useState<SettingNavigation | { name: string }>({ name: '' })
     useEffect(() => {
@@ -28,11 +29,39 @@ const InviteBySMS = (): JSX.Element => {
             <NavigationBar title={(currNavigation as { name: string }).name} callback={() => {
                 navigation.goBack()
             }} />
-            <ScrollView
-                bounces={false}
-                showsVerticalScrollIndicator={false}
-            >
-            </ScrollView>
+            <View style={{
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%'
+            }}>
+                <Image style={{
+                    width: 72,
+                    height: 72
+                }} source={require('../../../../../assets/images/sms.png')} />
+                <Text style={{
+                    fontWeight: '600',
+                    fontSize: 22,
+                    marginVertical: 15,
+                }}>Invite Join Instagram</Text>
+                <Text
+                    style={{
+                        width: SCREEN_WIDTH * 0.9,
+                        textAlign: 'center',
+                        color: '#666'
+                    }}>Send SMS to your friends to invite them join Instagram , discover Instagram content and create your network.</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        Linking.openURL(`sms:&body=Hello, I'm on Instagram as @${user?.username}. Install the app to follow my photos and videos.`);
+                    }}
+                    style={styles.btnSendSMS}>
+                    <Text style={{
+                        fontWeight: "bold",
+                        fontSize: 16,
+                        color: "#fff"
+                    }}>Send SMS</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -41,14 +70,17 @@ export default InviteBySMS
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff'
-    },
-    settingItem: {
-        height: 50,
-        width: '100%',
         backgroundColor: '#fff',
-        paddingHorizontal: 15,
-        flexDirection: 'row',
-        alignItems: 'center'
+        width: '100%',
+        height: '100%',
+    },
+    btnSendSMS: {
+        height: 44,
+        width: SCREEN_WIDTH * 0.9,
+        backgroundColor: "#318bfb",
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15
     }
 })
